c40b1f9f4b1a750c5f6d04c5a863d986c1901744
Lazar Stojanović <lakisd98@gmail.com>
2022-11-15 14:06:18 +0100

    Mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      customWhite: "hsl(0, 0%, 100%)",
+      lightGray: "hsl(212, 45%, 89%)",
+      grayishBlue: "hsl(220, 15%, 55%)",
+      darkBlue: "hsl(218, 44%, 22%)"
+    },
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
